319112dad0b74b4f54195f2f60657560577ba00e
Alberto Cristancho <albertocristancho@outlook.com>
2024-06-23 22:23:28 -0500

    feat: add notification feedback and spinner with backdrop

--- src/modules/auth/pages/singInPage/index.tsx
+++ src/modules/auth/pages/singInPage/index.tsx
@@ -1,13 +1,24 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
+import {
+  Backdrop,
+  Box,
+  Button,
+  CircularProgress,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { login } from "modules/auth/services/auth.services";
 import { Route } from "core/enums";
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { setUserInformation } from "core/utils/localStorage.utils";
+
 import { AppContext } from "core/context";
 export const SingInPage = (): JSX.Element => {
-  const [data, setData] = useState<{ email: string; password: string }>(Object({}));
-  const { setIsAuthenticated } = useContext(AppContext);
+  const [data, setData] = useState<{ email: string; password: string }>(
+    Object({})
+  );
+  const [isLoading, setIsLoading] = useState(false); // Loading state
+  const { setIsAuthenticated, setNotification } = useContext(AppContext);
   const navigate = useNavigate();
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -17,23 +28,51 @@ export const SingInPage = (): JSX.Element => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    login(data).then(({ data }) => {
-      if (data) {
-        setUserInformation(data);
-        setIsAuthenticated(true);
-        navigate(Route.Dashboard);
-      }
-    });
+    setIsLoading(true);
+    login(data)
+      .then(({ data }) => {
+        if (data) {
+          setUserInformation(data);
+          setIsAuthenticated(true);
+          navigate(Route.Dashboard);
+        }
+        if (!data) {
+          setNotification({
+            message: "Datos erroneos, intente nuevamente",
+            severity: "error",
+          });
+        }
+      })
+      .catch(() => {
+        console.log("Error");
+        setNotification({
+          message: "Error de servidor, intente nuevamente",
+          severity: "error",
+        });
+      })
+      .finally(() => setIsLoading(false));
   };
 
   return (
-    <Box display="flex" alignItems="center" flexDirection="column" paddingTop={10} gap={4}>
+    <Box
+      display="flex"
+      alignItems="center"
+      flexDirection="column"
+      paddingTop={10}
+      gap={4}
+    >
       <Box>
-        <Typography fontSize="24px" fontWeight="bold" textAlign="center" gutterBottom>
+        <Typography
+          fontSize="24px"
+          fontWeight="bold"
+          textAlign="center"
+          gutterBottom
+        >
           Inicio de Sesión
         </Typography>
         <Typography fontSize="20px" maxWidth={450} textAlign={"center"}>
-          Por favor ingrese su correo electronico y contraseña para iniciar sesión
+          Por favor ingrese su correo electronico y contraseña para iniciar
+          sesión
         </Typography>
       </Box>
 
@@ -58,9 +97,15 @@ export const SingInPage = (): JSX.Element => {
           name="password"
           onChange={handleChange}
         />
-        <Button variant="contained" fullWidth type="submit">
-          Iniciar Sesión
-        </Button>
+        {isLoading ? (
+          <Backdrop open={isLoading}>
+            <CircularProgress color="inherit" />
+          </Backdrop>
+        ) : (
+          <Button variant="contained" fullWidth type="submit">
+            Iniciar Sesión
+          </Button>
+        )}
       </Box>
     </Box>
   );
